7010a71dc345a0bac6d64707aa756dd960a54781
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-08-20 19:32:29 +0200

    created variables started bulding the form

---
+++ MultiStepForm/src/components/StepMenu.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="Wrapper">
+  <div class="Step-Wrapper" v-for="step in steps" :key="step.number">
+    <div class="Step-Wrapper__number">{{step.number}}</div>
+    <div class="Step-Wrapper__step">step {{step.number}}</div>
+    <div class="Step-Wrapper__info">{{step.info}}</div>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  const steps = ref([
+    {
+      number: 1,
+      info: 'your info'
+    },
+    {
+      number: 2,
+      info: 'select plan'
+    },
+    {
+      number: 3,
+      info: 'add-ons'
+    },
+    {
+      number: 4,
+      info: 'summary'
+    }
+      ]
+  )
+
+</script>
+
+<style scoped>
+@import "@/assets/StepMenu.css";
+</style>
--- MultiStepForm/src/views/HomeView.vue
+++ MultiStepForm/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
   <main>
-
+    <StepMenu/>
   </main>
 </template>
 
 <script setup lang="ts">
 
+import StepMenu from "@/components/StepMenu.vue";
 </script>
